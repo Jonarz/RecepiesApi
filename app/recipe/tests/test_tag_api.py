70f3125a870093c36969ab195e1903b27502bfe6
Jonarz <ramirez.jonathan.18@gmail.com>
2020-08-10 22:44:48 -0400

    ADD: create tag feature

--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -53,3 +53,19 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]["name"], tag.name)
+
+    def test_create_tag_succesful(self):
+        payload = {"name": "Test tag"}
+        self.client.post(TAGS_URL, payload)
+
+        exist = Tag.objects.filter(user=self.user, name=payload["name"])
+
+        self.assertTrue(exist)
+
+    def test_create_tag_invalid(self):
+        """Test creting a new tag with invalid payload"""
+        payload = {"name": ""}
+
+        response = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -10,7 +9,9 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
+class TagViewSet(
+    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin
+):
     """Manage tags in the database"""
 
     authentication_classes = (TokenAuthentication,)
@@ -21,3 +22,7 @@ class TagViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """ Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by("-name")
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        serializer.save(user=self.request.user)
